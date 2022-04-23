b89f1ac90a02e81a86fd69df9561969abfcc8677
Ashton Little <ashtonlittle@Ashtons-iMac.columbus.rr.com>
2022-04-23 10:57:27 -0400

    solution 2

--- LeetCoderomanintegerSolutions.js
+++ LeetCoderomanintegerSolutions.js
@@ -27,3 +27,43 @@ var romanToInt = function(s) {
     //below means return result
     return res
 };
+
+//SOLUTION 2
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+//Setting up constant map
+const map ={
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
+var romanToInt = function(s) {
+    //let sum be equal to zero
+    let sum = 0;
+    //for loop which is saying in english let i be = 0
+    //i is less than s(as defined above) length then add one
+    for (let i = 0; i < s.length; i++){
+        //this is saying let sign be equal to 1
+        let sign = 1
+        //this is saying if I is less than s.length and then (map[s[i + 1]] is equal to
+        //map[s[i]] * 5 OR
+        // map[s[i + 1]]  is equal to map[s[i]] * 10))
+        if ( i < s.length -1 && (map[s[i + 1]] === map[s[i]] * 5 || map[s[i + 1]] === map[s[i]] * 10)){
+            // this is saying sign times or = to negative one
+            sign *= -1
+        }
+        //this is saying the sum plus or equal to times map[s[i]]
+        sum += sign * map[s[i]]
+    }
+    //return sum
+    return sum
+};
